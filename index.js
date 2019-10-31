657c9b090673978363fbf4099ffd6667550185dc
Nimesh Solanki <nimesh3536@gmail.com>
2019-10-31 14:53:55 +0530

    bug fix: works with dynamic file, remove hardcoded fileName

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const readXlsxFile = require('read-excel-file/node');
 const fs = require('fs');
-let fileName = 'test.xlsx';
-// if (!fileName) {
-//     fileName = process.argv[2];
-//     // console.log(fileName);
-// }
-readXlsxFile(fileName).then((rows) => {
+// let fileName = process.env.FILE_NAME;
+let fileName = process.argv[2];
+console.log({ fileName });
+
+console.log(`process.argv: ${process.argv}`);
+readXlsxFile(fileName)
+  .then(rows => {
     const allParams = [];
     let numberOfRows = rows[0].length;
     for (let i = 1; i < rows.length; i++) {
-        var params = {};
-        // if (rows[i][0]) {
-        for (let j = 0; j < numberOfRows; j++) {
-            console.log(`rows[0][j] => ${rows[0][j]}`);
-            console.log(`rows[i][j] => ${rows[i][j]}`);
-            params[rows[0][j]] = rows[i][j]
-        }
-        // }
-        allParams.push(params)
+      var params = {};
+      for (let j = 0; j < numberOfRows; j++) {
+        params[rows[0][j]] = rows[i][j];
+      }
+      allParams.push(params);
     }
     return allParams;
-})
-    .then((allParams) => {
-        // console.log(JSON.stringify(allParams));
-        fs.writeFile("data.json", JSON.stringify(allParams), (err) => {
-            if (err) console.log(err);
-            else console.log(`wrote succesfully`);
-        })
-    }); 
+  })
+  .then(allParams => {
+    // console.log(JSON.stringify(allParams));
+    fs.writeFile('./data/data1.json', JSON.stringify(allParams), err => {
+      if (err) console.log(err);
+      else console.log(`wrote succesfully`);
+    });
+  });
